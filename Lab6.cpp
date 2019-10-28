5dc0cb04a9ebc4f9454eee2c04c52e17d78a4be9
PetroRealByk <petr.minailyuk@gmail.com>
2019-10-28 22:11:53 +0300

    changed name of a variable and then some constants

--- Lab6.cpp
+++ Lab6.cpp
@@ -5,14 +5,14 @@ int main()
 {
     int i,x;
     float Y;
-    int IDIDIT = 0;
+    int HowMany = 0;
     setlocale(0, "");
     cout << "Endter x" << endl;
     cin >> x;
-    for (i = 1; i <= 6; i++) {
-        IDIDIT = IDIDIT + pow(i,3);
+    for (i = 1; i <= 9; i++) {
+        HowMany = HowMany + pow(i,3);
     };
-    Y = (log(IDIDIT) + 2*pow(sin(x),2))/(x*x + 5);
+    Y = (log(HowMany) + 3*pow(sin(x),2))/(x*x + 5);
     cout << "Y = " << Y << endl;
     system("pause");
     return 0;
